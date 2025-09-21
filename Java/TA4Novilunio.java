5814f5a5b37ac226bc03f400020095cb88a2a416
NeolithicGaming <christophergumball@gmail.com>
2025-09-21 10:25:51 +0800

    TA4 Arrays and shiz

---
+++ Java/TA4Novilunio.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+import.java.util.Scanner;
+
+public class TA4Novilunio {
+   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+   } 
+}
